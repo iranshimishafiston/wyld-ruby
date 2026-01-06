f6aa1fecc1d4c291642a0fd6fd39a64bf70e5067
rudra <goutam.prasad.2707@gmail.com>
2026-01-06 22:39:30 +0530

    chores : Add public assets and icons

---
+++ public/index.ts
@@ -0,0 +1,7 @@
+import wyld_1 from "./assets/wyld_1.webp";
+import wyld_2 from "./assets/wyld_2.webp";
+import wyld_3 from "./assets/wyld_3.webp";
+import wyld_4 from "./assets/wyld_4.webp";
+import wyld_5 from "./assets/wyld_5.webp";
+
+export { wyld_1, wyld_2, wyld_3, wyld_4, wyld_5 };
